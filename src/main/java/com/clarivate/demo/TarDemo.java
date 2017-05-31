b175e04a6e2409622fa61f6c87817745ed9adc6a
richardxin <richardxin168@yahoo.com>
2017-05-31 14:10:20 -0700

    update

--- src/main/java/com/clarivate/demo/TarDemo.java
+++ src/main/java/com/clarivate/demo/TarDemo.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-
+import java.io.StringWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,12 +13,28 @@ import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+
 public class TarDemo {
 	//private TarArchiveInputStream tarArchiveInputStream;
 	private static Pattern recordIdPattern = Pattern.compile("([^/]+)\\.json$");
+	
+	private static boolean print2Console = true;
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String... args) throws FileNotFoundException {
 		String filePath = "data/drug-2016-11-14_1519-all-pp.json.tar.gz";
+		for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "--path":
+                	filePath = args[++i];
+                    break;
+                case "--print-to-console":
+                	print2Console = Boolean.parseBoolean(args[++i]);
+             }
+		}
+		
 		FileInputStream fileInputStream = new FileInputStream(filePath);
 
 		TarDemo demo = new TarDemo();
@@ -49,8 +65,9 @@ public class TarDemo {
 					String recordId = extractId(entry.getName());
 					byte[] ba = new byte[(int) entry.getSize()];
 					tarArchive.read(ba, 0, ba.length);
-					String currentValue = new String(ba);
-					System.out.println(counter + ":" + recordId + ":" + currentValue);
+					String currentValue = validateAndRemoveWhitespace(new String(ba));
+					if (print2Console)
+						System.out.println(counter + ":" + recordId + ":" + currentValue);
 					counter++;
 				}
 				entry = tarArchive.getNextTarEntry();
@@ -62,4 +79,21 @@ public class TarDemo {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	private String validateAndRemoveWhitespace(String jsonInput)  {
+        try {
+            StringWriter writer  = new StringWriter();
+            JsonFactory factory = new JsonFactory();
+            JsonParser parser = factory.createParser(jsonInput);
+            try (JsonGenerator gen = factory.createGenerator(writer)) {
+                while (parser.nextToken() != null) {
+                    gen.getCurrentValue();
+                    gen.copyCurrentEvent(parser);
+                }
+            }
+            return writer.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Invalid JSON="+jsonInput,e);
+        }
+    }
 }
